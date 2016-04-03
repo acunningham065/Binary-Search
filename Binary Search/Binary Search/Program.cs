a1acae340b750aa81b2ea84ff352d55e53a76808
acunningham065 <acunningham065@gmail.com>
2016-04-03 19:57:07 +0100

    Add files via Upload

---
+++ Binary Search/Binary Search/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare array and target
+            int[] numbers = new int[10];
+            int target = 0;
+            bool targetFound = false;
+
+            //Set first, mid and last
+            int first = 0;
+            int mid = 0;
+            int last = numbers.Length - 1;
+
+            while (first <= last)
+            {
+                //Calculate mid
+                mid = (first + last) / 2;
+
+                //check if target is mid
+                if (target == numbers[mid])
+                {
+                    Console.WriteLine("Target found at position " + mid.ToString());
+
+                    targetFound = true;
+
+                    //Break out of loop
+                    break;
+                }
+                else if (target < numbers[mid])
+                {
+                    last = mid - 1;
+                }
+                else if (target > numbers[mid])
+                {
+                    first = mid + 1;
+                }
+
+            }//End While
+
+
+            if (!targetFound)
+            {
+                
+            }
+
+
+
+        }//End Main
+    }
+}
